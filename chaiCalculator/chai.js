1fa9e9913fb7ce3f28018c62ec1c7ae825d43864
Umurowoto <167550843+Umurowoto@users.noreply.github.com>
2025-06-12 09:57:43 +0000

    code 1 folders and files

---
+++ chaiCalculator/chai.js
@@ -0,0 +1,14 @@
+function calculateBodaFare(){
+    let kmInput = prompt("Unafika wapi Mkubwa? kilometer ngapi?: ")
+    let distanceInKm = Number(kmInput);
+    let baseFare = 50;
+    let chargePerKm =15;
+    let totalFare =baseFare + (distanceInKm * chargePerKm);
+
+    console.log("Uko kwote? Io ni " + distanceInKm + "km:")
+    console.log("Ukikalia Pikipiki:" + " KES" + baseFare)
+    console.log("Mpaka Uko:" + " KES" + totalFare)
+    console.log("Total:" + " KES" + totalFare)
+    console.log("Panda Pikipiki!")
+}
+calculateBodaFare();    
---
+++ mobilemoneyestimator/fee.js
@@ -0,0 +1,18 @@
+function estimateTransactionFee(){
+    let input =prompt("Unatuma Ngapi? (KES):")
+    let amountToSend = Number(input)
+    let fee = amountToSend * 0.015;
+
+    if(fee < 10){
+        fee = 10;
+    }else if(fee > 70){
+        fee = 70;
+    }
+    let totalAmount = amountToSend + fee;
+
+    console.log("Sending KES " + amountToSend + ":")
+console.log("Calculated Transaction Fee: KES" + fee)
+console.log("Total amount to be debited: KES " + totalAmount)
+console.log("Send Money Securely!")
+}
+estimateTransactionFee();
